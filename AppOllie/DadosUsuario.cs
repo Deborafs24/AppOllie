9845e006e99c211604c66a022641c9d741f13adc
Usuario <Usuario@CPD01-M01>
2024-08-13 09:40:01 -0300

    Banco de Dados

---
+++ AppOllie/DadosUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppOllie
+{
+    public class DadosUsuario
+    {
+        public string Email { get; set; }
+
+        public string senha { get; set; }
+    }
+}
--- AppOllie/Views/PagInicial.xaml.cs
+++ AppOllie/Views/PagInicial.xaml.cs
@@ -7,9 +7,38 @@ public partial class PagInicial : ContentPage
 		InitializeComponent();
 	}
 
-    private async void acesso_Btn_Clicked(object sender, EventArgs e)
+    private void acesso_Btn_Clicked(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new Views.Permissoes());
+        try
+        {
+            List<DadosUsuario> lista_usuarios = new List<DadosUsuario>()
+            {
+                new DadosUsuario()
+                {
+                    Email = "valeria@gmail.com",
+                    senha = "1234"
+                }
+            };
+
+            DadosUsuario dados_digitados = new DadosUsuario()
+            {
+                Email = txt_email.Text,
+                senha = txt_senha.Text
+            };
+
+            if(lista_usuarios.Any(
+                i => dados_digitados.Email == i.Email &&
+                     dados_digitados.senha == i.senha))
+            {
+
+            }
+
+        } catch(Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "Fechar");
+        }
+
+        // await Navigation.PushAsync(new Views.Permissoes());
     }
 
     private void esqueci_Btn_Clicked(object sender, EventArgs e)
